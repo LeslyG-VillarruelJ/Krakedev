a356a6ec6765869c4131839236632517b554acc3
LeslyG-VillarruelJ <leslyvillarruel90@gmail.com>
2024-01-26 17:50:25 -0500

    funcion mostrarTexto implementada

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -3,6 +3,14 @@ saludar = function(){
     let apellido = recuperarTexto("txtApellido");
     let edad = recuperarInt("txtEdad");
     let estatura = recuperarFloat("txtEstatura");
+    //mostrar el mensaje con la funcion mostrarTexto
+    let mensajeBienvenida = "Bienvenido " + nombre + " " + apellido;
+    mostrarTexto("lblResultado",mensajeBienvenida);
+}
+
+mostrarTexto = function(idComponente,mensaje){
+    let componente = document.getElementById(idComponente);
+    componente.innerText = mensaje;
 }
 
 recuperarTexto = function(idComponente){
