4bc408ec196afeecd71f61727acb9c181665639d
LeslyG-VillarruelJ <leslyvillarruel90@gmail.com>
2024-01-26 11:50:34 -0500

    el boton modifica la imagen

--- EjercicioGit/ejercicio.js
+++ EjercicioGit/ejercicio.js
@@ -1,3 +1,7 @@
 saludar = function(){
+    let cmpImagenSaludo;
+    cmpImagenSaludo = document.getElementById("imgSaludo");
+    cmpImagenSaludo.src = "./imagenes/ganar.gif"
     console.log("ingresa a saludar");
+
 }
