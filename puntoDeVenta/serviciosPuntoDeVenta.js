fca9c112792988b3b2f94222561359142589a2b2
LeslyG-VillarruelJ <leslyvillarruel90@gmail.com>
2024-01-27 21:11:01 -0500

    Version inicial, funciones implementadas y mostrar subtotal implementado correctamente

---
+++ puntoDeVenta/serviciosPuntoDeVenta.js
@@ -0,0 +1,22 @@
+calcularValorDescuento = function(monto,porcentajeDescuento){
+    let descuento;
+    descuento = (monto * porcentajeDescuento)/100;
+    return descuento;
+}
+
+calcularIVA = function(monto){
+    let iva;
+    iva = (monto * 12)/100;
+    return iva;
+}
+
+calcularSubtotal = function(precio,cantidad){
+    let subtotal;
+    subtotal = precio * cantidad;
+    return subtotal;
+}
+
+calcularTotal = function(subtotal,descuento,iva){
+    let total;
+    total = subtotal - descuento + iva;
+}
