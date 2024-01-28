9ffaa629597a559617e28bc5233ad7273ac1c66e
LeslyG-VillarruelJ <leslyvillarruel90@gmail.com>
2024-01-27 20:35:57 -0500

    Funcion mostrarTextoEnCaja inplementada

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -8,6 +8,8 @@ saludar = function(){
     mostrarTexto("lblResultado",mensajeBienvenida);
     //mostrar Imagen
     mostrarImagen("imgSaludo","./imagenes/saludo.gif")
+    //limpiar la caja de texto con la funcion mostrarTextoEnCaja
+    mostrarTextoEnCaja("txtNombre","");
 }
 
 mostrarImagen = function(idComponente,rutaImagen){
@@ -18,6 +20,11 @@ mostrarImagen = function(idComponente,rutaImagen){
 mostrarTexto = function(idComponente,mensaje){
     let componente = document.getElementById(idComponente);
     componente.innerText = mensaje;
+}
+
+mostrarTextoEnCaja = function(idComponente,mensaje){
+    let componente = document.getElementById(idComponente);
+    componente.value = mensaje;
 }
 
 recuperarTexto = function(idComponente){
