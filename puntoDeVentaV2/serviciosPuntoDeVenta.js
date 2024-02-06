6afaa1a005e21cf45ddf2298374f96b86e0bace1
LeslyG-VillarruelJ <leslyvillarruel90@gmail.com>
2024-02-06 17:27:49 -0500

    Modificaciones en ejercicioPuntoDeVentaV2 con reubicacion de metodos de las validaciones

--- puntoDeVentaV2/serviciosPuntoDeVenta.js
+++ puntoDeVentaV2/serviciosPuntoDeVenta.js
@@ -21,3 +21,68 @@ calcularTotal = function (subtotal, descuento, iva) {
     total = subtotal - descuento + iva;
     return total;
 }
+
+calcularDescuentoPorVolumen = function (subtotal, cantidad) {
+    let descuento;
+    if (cantidad < 3) {
+        descuento = 0;
+    } else if (cantidad >= 3 && cantidad <= 5) {
+        descuento = subtotal * 0.03;
+    } else if (cantidad >= 6 && cantidad <= 11) {
+        descuento = subtotal * 0.04;
+    } else if (cantidad >= 12) {
+        descuento = subtotal * 0.05;
+    }
+    return descuento;
+}
+
+esProductoValido = function (producto, idComponenteError) {
+    let valida = true;
+    if (producto != "") {
+        if (producto.length > 10) {
+            mostrarTexto(idComponenteError, "No puede tener mas de 10 caracteres");
+            valida = false;
+        }
+    } else {
+        mostrarTexto(idComponenteError, "CAMPO OBLIGATORIO");
+        valida = false;
+    }
+    if (valida == true) {
+        mostrarTexto(idComponenteError, "");
+    }
+    return valida;
+}
+
+esCantidadValida = function (cantidad, idComponenteError) {
+    let correcto = true;
+    if (!isNaN(cantidad)) {
+        if (cantidad < 0 || cantidad > 100) {
+            mostrarTexto(idComponenteError, "Debe ser un entero entre 0 y 100");
+            correcto = false;
+        }
+    } else {
+        mostrarTexto(idComponenteError, "CAMPO OBLIGATORIO");
+        correcto = false;
+    }
+    if (correcto == true) {
+        mostrarTexto(idComponenteError, "");
+    }
+    return correcto;
+}
+
+esPrecioValido = function (precio, idComponenteError) {
+    let valido = true;
+    if (!isNaN(precio)) {
+        if (precio < 0 || precio > 50) {
+            mostrarTexto(idComponenteError, "Debe ser un numero entre 0 y 50");
+            valido = false;
+        }
+    } else {
+        mostrarTexto(idComponenteError, "CAMPO OBLIGATORIO");
+        valido = false;
+    }
+    if (valido == true) {
+        mostrarTexto(idComponenteError, "");
+    }
+    return valido;
+}
