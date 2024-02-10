6db50aaacb6ad823e42127122ed35974c9c64295
LeslyG-VillarruelJ <leslyvillarruel90@gmail.com>
2024-02-10 07:20:55 -0500

    Modificacion, si el jugador o computador tiene 5 puntos mensajey boton nueva partida, con casos de prueba

--- juego/juego.js
+++ juego/juego.js
@@ -5,7 +5,7 @@ jugar = function (seleccionado) {
     let resultadoComputadora;
     let ruta;
     let valorPartida;
-    let resultadoPartida
+    let resultadoPartida;
     resultadoComputadora = generarElemento();
     ruta = generarRuta(resultadoComputadora);
     mostrarImagen("imgComputadora", ruta);
@@ -15,12 +15,28 @@ jugar = function (seleccionado) {
     } else if (valorPartida == 1) {
         resultadoPartida = "PERDISTE LA PARTIDA!!";
         puntosComputador = puntosComputador + 1;
-        mostrarTexto("puntajeComputadora",puntosComputador);
+        mostrarTexto("puntajeComputadora", puntosComputador);
     } else {
         resultadoPartida = "GANASTE LA PARTIDA!!"
-        puntosUsuario = puntosUsuario + 1;        
-        mostrarTexto("puntajeUsuario",puntosUsuario);
+        puntosUsuario = puntosUsuario + 1;
+        mostrarTexto("puntajeUsuario", puntosUsuario);
     }
     //mostrar en pantalla el resultado
     mostrarTexto("lblResultado", resultadoPartida);
+    // se modificara cuando se cumpla la condicion se mostrara un mensaje diferente
+    if (puntosUsuario == 5) {
+        mostrarTexto("lblFinPartida", "HAS GANADO EL JUEGO");
+    } else if (puntosComputador == 5) {
+        mostrarTexto("lblFinPartida", "EL COMPUTADOR TE HA VENCIDO");
+    } 
+}
+
+limpiar = function(){
+    puntosComputador = 0;
+    puntosUsuario = 0;
+    mostrarImagen("imgComputadora", "");
+    mostrarTexto("puntajeComputadora", puntosComputador);
+    mostrarTexto("puntajeUsuario", puntosUsuario);
+    mostrarTexto("lblResultado", "");
+    mostrarTexto("lblFinPartida", "");
 }
