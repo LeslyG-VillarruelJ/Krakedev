7ecddf6e31d8db5462add83be4b2772fe6733422
LeslyG-VillarruelJ <leslyvillarruel90@gmail.com>
2024-02-14 17:25:28 -0500

    Paso 1 - Ingresar palabra secreta completado correctamente

--- Ahorcado/ahorcado.js
+++ Ahorcado/ahorcado.js
@@ -1,4 +1,7 @@
 //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
+//variables globales
+let palabraSecreta;
+
 esMayuscula = function (caracter) {
     let codigoCaracter = caracter.charCodeAt(0);
     if (codigoCaracter >= 65 && codigoCaracter <= 90) {
@@ -6,4 +9,22 @@ esMayuscula = function (caracter) {
     } else {
         return false;
     }
+}
+
+guardarPalabra = function(){
+    let palabra = recuperarTexto("password");
+    let caracter;
+    let validacionMayuscula = true;
+    for(let i=0;i<palabra.length;i++){
+        caracter = palabra.charAt(i);
+        if(!esMayuscula(caracter)){
+            validacionMayuscula = false;
+        }
+    }
+    if(palabra.length == 5 && validacionMayuscula==true){
+        palabraSecreta = palabra;
+    }else{
+        alert("Debe ingresar una palabra de 5 letras mayusculas");
+    }
+    console.log(palabra);
 }
