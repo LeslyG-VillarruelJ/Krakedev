207a000966f1fef01e4109c24fedf657829da385
LeslyG-VillarruelJ <leslyvillarruel90@gmail.com>
2024-02-14 21:16:55 -0500

    Paso 6 - funcion mostrarhorcado funcionando

--- Ahorcado/ahorcado.js
+++ Ahorcado/ahorcado.js
@@ -60,6 +60,7 @@ validar = function (letra) {
     if (letrasEncontradas == 0) {
         alert("LA LETRA NO ES PARTE DE LA PALABRA");
         errores++;
+        mostrarAhorcado();
     }
 }
 
@@ -68,13 +69,35 @@ ingresarLetra = function () {
     letra = recuperarTexto("txtLetra");
     if (esMayuscula(letra)) {
         validar(letra);
+        intentos++;
         if (coincidencias == 5) {
             alert("HA GANADO");
         } else if (intentos == 10) {
             alert("HA PERDIDO");
         }
-        intentos++;
     } else {
         alert("SOLO SE ACEPTAN MAYUSCULAS");
+    }
+}
+
+mostrarAhorcado = function(){
+    if(errores == 1){
+        mostrarImagen("ahorcadoImagen", "Ahorcado_01.png");
+    }else if(errores == 2){
+        mostrarImagen("ahorcadoImagen", "Ahorcado_02.png");
+    }else if(errores == 3){
+        mostrarImagen("ahorcadoImagen", "Ahorcado_03.png");
+    }else if(errores == 4){
+        mostrarImagen("ahorcadoImagen", "Ahorcado_04.png");
+    }else if(errores == 5){
+        mostrarImagen("ahorcadoImagen", "Ahorcado_05.png");
+    }else if(errores == 6){
+        mostrarImagen("ahorcadoImagen", "Ahorcado_06.png");
+    }else if(errores == 7){
+        mostrarImagen("ahorcadoImagen", "Ahorcado_07.png");
+    }else if(errores == 8){
+        mostrarImagen("ahorcadoImagen", "Ahorcado_08.png");
+    }else if(errores == 9){
+        mostrarImagen("ahorcadoImagen", "Ahorcado_09.png");
     }
 }
