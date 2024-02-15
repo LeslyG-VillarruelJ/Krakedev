29c2db4c592802fc3beb9ac3045f35efa61af565
LeslyG-VillarruelJ <leslyvillarruel90@gmail.com>
2024-02-15 10:45:19 -0500

    Tablas en HTML, con arreglos y javascript

--- arreglos/ejercicio1.js
+++ arreglos/ejercicio1.js
@@ -13,6 +13,7 @@ probarAgregar = function () {
 
 agregarNota = function (nota) {
     notas.push(nota);
+    mostrarNotas();
 }
 
 recorrerArreglo = function () {
@@ -37,4 +38,27 @@ ejecutarPromedio = function () {
     let resultado;
     resultado = calcularPromedio();
     mostrarTexto("lblPromedio", "PROMEDIO = " + resultado);
+}
+
+//generar Tablas desde javaScript
+generarTabla = function () {
+    let contenidoTabla = "";
+    let cmpTabla = document.getElementById("divTabla");
+    contenidoTabla += "<table><tr><td>UNO</td></tr>" + "<tr><td>DOS</td></tr></table>";
+    cmpTabla.innerHTML = contenidoTabla;
+}
+
+mostrarNotas = function () {
+    let contenidoTabla = "";
+    let cmpTabla = document.getElementById("divTabla");
+    contenidoTabla = "<table><tr><th>NOTA</th></tr>";
+    let miNota;
+    for (let i = 0; i < notas.length; i++) {
+        miNota = notas[i];
+        contenidoTabla += "<tr><td>";
+        contenidoTabla += miNota;
+        contenidoTabla += "</tr></td>";
+    }
+    contenidoTabla += "</table>"
+    cmpTabla.innerHTML = contenidoTabla;
 }
