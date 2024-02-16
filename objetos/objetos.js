e18f49baf73de7b47679bcb7d3f07d3ca0c66e94
LeslyG-VillarruelJ <leslyvillarruel90@gmail.com>
2024-02-16 11:52:54 -0500

    Modificacion en objetos, utilizarlos como parametros

--- objetos/objetos.js
+++ objetos/objetos.js
@@ -27,12 +27,62 @@ crearObjeto = function () {
         stock: 15
     }
     console.log(producto1.nombre);
-    console.log(producto2.nombre);
+    console.log(producto2.precio);
     if (producto1.stock > producto2.stock) {
         console.log("Producto 1 tiene mayor stock");
     } else if (producto1.stock < producto2.stock) {
         console.log("Producto 2 tiene mayor stock");
     } else {
         console.log("Ambos productos tienen el mismo stock");
+    }
+}
+
+crearCliente = function () {
+    let cliente = {
+        cedula: "1234567890",
+        nombre: "Juan"
+    }
+    let cliente1 = {};
+    cliente1.nombre = "Romeo";
+    cliente1.apellido = "Salcedo";
+    cliente1.cedula = "0987654321";
+}
+
+incrementarSaldo = function (cuenta, monto) {
+    cuenta.saldo += monto;
+}
+
+probarIncrementoSaldo = function () {
+    let cta = {
+        numero: "123456",
+        saldo: 100
+    }
+    incrementarSaldo(cta, 100);
+    console.log(cta.saldo);
+}
+
+determinarMayor = function (person1, persona2) {
+    if (person1.edad > persona2.edad) {
+        return person1;
+    } else if (person1.edad < persona2.edad) {
+        return persona2;
+    } else {
+        return null;
+    }
+}
+
+probarDeterminarMayor = function () {
+    let persona1 = {
+        nombre: "Daniel",
+        edad: 45
+    }
+    let persona2 = {
+        nombre: "Luisa",
+        edad: 48
+    }
+    let mayor;
+    mayor = determinarMayor(persona1, persona2); // retorna el objeto como tal
+    if (mayor != null) {
+        console.log("El mayor es: " + mayor.nombre);
     }
 }
