e67f6e63acd0fb4f6b27529a867dc411b4c2750c
LeslyG-VillarruelJ <leslyvillarruel90@gmail.com>
2024-02-17 10:25:49 -0500

    Roles Parte 2 - Mostrar empleados en una tabla

--- roles/roles.js
+++ roles/roles.js
@@ -1,12 +1,14 @@
 let empleados = [
     { cedula: "1714616123", nombre: "John", apellido: "Cena", sueldo: 500.0 },
-    { cedula: "0914632123", nombre: "Luisa", apellido: "Gonzalez", sueldo: 900.0 }
+    { cedula: "0914632123", nombre: "Luisa", apellido: "Gonzalez", sueldo: 900.0 },
+    { cedula: "1900208641", nombre: "David", apellido: "Perez", sueldo: 800.0}
 ]
 
 mostrarOpcionEmpleado = function () {
     mostrarComponente("divEmpleado");
     ocultarComponente("divRol");
     ocultarComponente("divResumen");
+    mostrarEmpleados();
 }
 
 mostrarOpcionRol = function () {
@@ -19,4 +21,28 @@ mostrarOpcionResumen = function () {
     mostrarComponente("divResumen");
     ocultarComponente("divEmpleado");
     ocultarComponente("divRol");
+}
+
+mostrarEmpleados = function(){
+    let cmpTablaEmpleados = document.getElementById("tablaEmpleados");
+    let empleado;
+    let tablaEmpleados = 
+    "<table>" 
+    + "<tr>"
+    + "<th>CEDULA</th>"
+    + "<th>NOMBRE</th>"
+    + "<th>APELLIDO</th>"
+    + "<th>SUELDO</th>"
+    + "</tr>";
+    for(let i=0; i<empleados.length; i++){
+        empleado = empleados[i];
+        tablaEmpleados += "<tr>";
+        tablaEmpleados += "<td>" + empleado.cedula + "</td>";
+        tablaEmpleados += "<td>" + empleado.nombre + "</td>";
+        tablaEmpleados += "<td>" + empleado.apellido + "</td>";
+        tablaEmpleados += "<td>" + empleado.sueldo + "</td>";
+        tablaEmpleados += "</tr>";
+    }
+    tablaEmpleados += "</table>";
+    cmpTablaEmpleados.innerHTML = tablaEmpleados;
 }
