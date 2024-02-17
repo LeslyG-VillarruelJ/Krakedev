5e1da7e9fb0e780da6ba9192ee061c3ab830fa3e
LeslyG-VillarruelJ <leslyvillarruel90@gmail.com>
2024-02-17 18:31:01 -0500

    Desabilitar componentes de divEmpleado y funcion para habilitar de nuevo los componentes. BOTON NUEVO

--- roles/roles.js
+++ roles/roles.js
@@ -1,14 +1,20 @@
 let empleados = [
     { cedula: "1714616123", nombre: "John", apellido: "Cena", sueldo: 500.0 },
     { cedula: "0914632123", nombre: "Luisa", apellido: "Gonzalez", sueldo: 900.0 },
-    { cedula: "1900208641", nombre: "David", apellido: "Perez", sueldo: 800.0}
+    { cedula: "1900208641", nombre: "David", apellido: "Perez", sueldo: 800.0 }
 ]
+let esNuevo = false;
 
 mostrarOpcionEmpleado = function () {
     mostrarComponente("divEmpleado");
     ocultarComponente("divRol");
     ocultarComponente("divResumen");
     mostrarEmpleados();
+    deshabilitarComponente("txtCedula");
+    deshabilitarComponente("txtNombre");
+    deshabilitarComponente("txtApellido");
+    deshabilitarComponente("txtSueldo");
+    deshabilitarComponente("btnGuardar");
 }
 
 mostrarOpcionRol = function () {
@@ -23,18 +29,18 @@ mostrarOpcionResumen = function () {
     ocultarComponente("divRol");
 }
 
-mostrarEmpleados = function(){
+mostrarEmpleados = function () {
     let cmpTablaEmpleados = document.getElementById("tablaEmpleados");
     let empleado;
-    let tablaEmpleados = 
-    "<table>" 
-    + "<tr>"
-    + "<th>CEDULA</th>"
-    + "<th>NOMBRE</th>"
-    + "<th>APELLIDO</th>"
-    + "<th>SUELDO</th>"
-    + "</tr>";
-    for(let i=0; i<empleados.length; i++){
+    let tablaEmpleados =
+        "<table>"
+        + "<tr>"
+        + "<th>CEDULA</th>"
+        + "<th>NOMBRE</th>"
+        + "<th>APELLIDO</th>"
+        + "<th>SUELDO</th>"
+        + "</tr>";
+    for (let i = 0; i < empleados.length; i++) {
         empleado = empleados[i];
         tablaEmpleados += "<tr>";
         tablaEmpleados += "<td>" + empleado.cedula + "</td>";
@@ -45,4 +51,13 @@ mostrarEmpleados = function(){
     }
     tablaEmpleados += "</table>";
     cmpTablaEmpleados.innerHTML = tablaEmpleados;
+}
+
+ejecutarNuevo = function () {
+    habilitarComponente("txtCedula");
+    habilitarComponente("txtNombre");
+    habilitarComponente("txtApellido");
+    habilitarComponente("txtSueldo");
+    habilitarComponente("btnGuardar");
+    esNuevo = true;
 }
