ee133af4f8d04a4912aeb3e974641fe559719f7f
LeslyG-VillarruelJ <leslyvillarruel90@gmail.com>
2024-02-17 23:30:11 -0500

    Crear empleado, con modificacon del boton guardar

--- roles/roles.js
+++ roles/roles.js
@@ -61,3 +61,111 @@ ejecutarNuevo = function () {
     habilitarComponente("btnGuardar");
     esNuevo = true;
 }
+
+buscarEmpleado = function (cedula) {
+    let empleadoEncontrado = null;
+    for (let i = 0; i < empleados.length; i++) {
+        if (empleados[i].cedula == cedula) {
+            empleadoEncontrado = empleados[i];
+        }
+    }
+    return empleadoEncontrado;
+}
+
+agregarEmpleado = function (empleado) {
+    let empleadoBuscado;
+    empleadoBuscado = buscarEmpleado(empleado.cedula);
+    if (empleadoBuscado == null) {
+        empleados.push(empleado);
+        return true;
+    } else {
+        return false;
+    }
+}
+
+guardar = function () {
+    let valorCedula = recuperarTexto("txtCedula");
+    let valorNombre = recuperarTexto("txtNombre");
+    let valorApellido = recuperarTexto("txtApellido");
+    let valorSueldo = recuperarFloat("txtSueldo");
+    let caracter;
+    let sinErrores = [true, true, true, true]; // con este arreglo veremos si ya no existe ningun error en todas las validaciones
+    //VALIDACION DE LA CEDULA
+    if (valorCedula.length != 10) {
+        sinErrores[0] = false;
+    }
+    for (let i = 0; i < valorCedula.length; i++) {
+        caracter = valorCedula.charCodeAt(i);
+        if (caracter < 48 || caracter > 57) {
+            sinErrores[0] = false;
+            break;
+        }
+    }
+    if (sinErrores[0]) {
+        mostrarTexto("lblErrorCedula", "");
+    } else {
+        mostrarTexto("lblErrorCedula", "Debe tener exactamente 10 digitos");
+    }
+    //VALIDACION DEL NOMBRE
+    if (valorNombre.length < 3) {
+        sinErrores[1] = false;
+    }
+    for (let i = 0; i < valorNombre.length; i++) {
+        caracter = valorNombre.charCodeAt(i);
+        if (caracter < 65 || caracter > 90) {
+            sinErrores[1] = false;
+            break;
+        }
+    }
+    if (sinErrores[1]) {
+        mostrarTexto("lblErrorNombre", "");
+    } else {
+        mostrarTexto("lblErrorNombre", "Debe tener al menos 3 letras mayusculas");
+    }
+    //VALIDACION DEL APELLIDO
+    if (valorApellido.length < 3) {
+        sinErrores[2] = false;
+    }
+    for (let i = 0; i < valorApellido.length; i++) {
+        caracter = valorApellido.charCodeAt(i);
+        if (caracter < 65 || caracter > 90) {
+            sinErrores[2] = false;
+            break;
+        }
+    }
+    if (sinErrores[2]) {
+        mostrarTexto("lblErrorApellido", "");
+    } else {
+        mostrarTexto("lblErrorApellido", "Debe tener al menos 3 letras mayusculas");
+    }
+    //VALIDACION SUELDO
+    if (isNaN(valorSueldo) == false) {
+        if (valorSueldo < 400 || valorSueldo > 5000) {
+            sinErrores[3] = false;
+        }
+    } else {
+        sinErrores[3] = false;
+    }
+    if (sinErrores[3]) {
+        mostrarTexto("lblErrorSueldo", "");
+    } else {
+        mostrarTexto("lblErrorSueldo", "Debe ser un numero decimal entre 400 y 5000");
+    }
+    //Verificacion para guardar el cliente
+    if (sinErrores[0] && sinErrores[1] && sinErrores[2] && sinErrores[3]) {
+        if (esNuevo == true) {
+            let empleadoNuevo = {};
+            let empleadoGuardado;
+            empleadoNuevo.cedula = valorCedula;
+            empleadoNuevo.nombre = valorNombre;
+            empleadoNuevo.apellido = valorApellido;
+            empleadoNuevo.sueldo = valorSueldo;
+            empleadoGuardado = agregarEmpleado(empleadoNuevo);
+            if (empleadoGuardado == true) {
+                alert("EMPLEADO GUARDADO CORRECTAMENTE");
+            } else {
+                alert("YA EXISTE UN EMPLEADO CON LA CEDULA " + empleadoNuevo.cedula);
+            }
+        }
+    }
+}
